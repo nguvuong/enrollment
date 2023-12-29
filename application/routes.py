2ff174f2b4adb5af6bdc7fe171c7bcfe4215dd32
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-29 04:47:53 -0500

    ccreated nav links and route patterns

--- application/routes.py
+++ application/routes.py
@@ -4,6 +4,22 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return render_template("index.html")
+    # try the if login function in index file
+    return render_template("index.html", login=False)
 
+@app.route("/login")
+def login():
+    # try the if login function in index file
+    return render_template("login.html", login=False)
+
+@app.route("/courses")
+def courses():
+    # try the if login function in index file
+    return render_template("courses.html", login=False)
+
+@app.route("/register")
+def register():
+    # try the if login function in index file
+    return render_template("register.html", login=False)
