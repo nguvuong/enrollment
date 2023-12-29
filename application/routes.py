861b9a969ed083d6ff3a530624bb7fbe1801a129
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-29 14:00:48 -0500

    use URL variables

--- application/routes.py
+++ application/routes.py
@@ -13,15 +13,17 @@ def login():
     # try the if login function in index file
     return render_template("login.html", login=True)
 
-@app.route("/courses")
-def courses():
+@app.route("/courses/")
+# use URL variable
+@app.route("/courses/<term>")
+def courses(term="Spring 2019"):
     # pass data to view 
     courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
     # print the data to the console 
     print(courseData[0]["title"])
     # try the if login function in index file
-    return render_template("courses.html", courseData = courseData, courses = True)
+    return render_template("courses.html", courseData = courseData, courses = True, term = term)
 
 @app.route("/register")
 def register():
