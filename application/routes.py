47fb602d40c6069fb7a684a707bf5b820fce164e
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-29 13:08:00 -0500

    pass data to view

--- application/routes.py
+++ application/routes.py
@@ -1,25 +1,29 @@
 from application import app
 from flask import render_template
 
-
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
     # try the if login function in index file
-    return render_template("index.html", login=False)
+    return render_template("index.html", index=True)
 
 @app.route("/login")
 def login():
     # try the if login function in index file
-    return render_template("login.html", login=False)
+    return render_template("login.html", login=True)
 
 @app.route("/courses")
 def courses():
+    # pass data to view 
+    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+
+    # print the data to the console 
+    print(courseData[0]["title"])
     # try the if login function in index file
-    return render_template("courses.html", login=False)
+    return render_template("courses.html", courseData = courseData, courses = True)
 
 @app.route("/register")
 def register():
     # try the if login function in index file
-    return render_template("register.html", login=False)
+    return render_template("register.html", register=True)
